28c3fe7ef289811d8f05adf2b73cca982374a0c5
Holger Brunn <hbrunn@therp.nl>
2018-01-22 17:04:16 +0100

    [FIX] import

--- base_active_date/models/active_date_test.py
+++ base_active_date/models/active_date_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright - 2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import fields, model
+from odoo import fields, models
 
 
 class ActiveDateTest(models.Model):
